05accdcc8adfa33a8265b4e1b208f7aefda4a432
shennan922 <shennan922@163.com>
2019-03-22 16:36:22 +0800

    commit installation series script

---
+++ autoscript/installation.sh
@@ -0,0 +1,31 @@
+path_to_tensorflow=$1
+
+#install tensorflow 1.9.0
+echo "start installing tensorflow 1.9.0"
+pip2 install --user tensorflow-1.9.0-cp27-cp27mu-linux_x86_64.whl
+
+#install dependencies using pip2
+echo "start installing Cython"
+pip2 install --user Cython
+echo "start installing contextlib2"
+pip2 install --user contextlib2
+echo "start installing pillow"
+pip2 install --user pillow
+echo "start installing lxml"
+pip2 install --user lxml
+echo "start installing jupyter"
+pip2 install --user jupyter
+echo "start installing matplotlib"
+pip2 install --user matplotlib
+
+#COCO API installation
+echo "start installing pycocotools folder"
+git clone https://github.com/cocodataset/cocoapi.git
+cd cocoapi/PythonAPI
+make
+cp -r pycocotools $path_to_tensorflow/models/research/
+
+# Protobuf Compilation .From tensorflow/models/research/
+echo "Protobuf Compilation"
+cd $path_to_tensorflow/models/research/
+protoc object_detection/protos/*.proto --python_out=.
